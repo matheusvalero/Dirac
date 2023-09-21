67f94e67ccf38d8fec95f0196f16af9b03d59e4b
Matheus Valero <matheusvalero99@gmail.com>
2023-09-21 16:50:11 -0300

    Up

--- Dirac.rs
+++ Dirac.rs
@@ -11,7 +11,20 @@ fn main() {
     
     let gamma_matrix = [gamma0, gamma1, gamma2, gamma3];
     
+    let gamma_matrix = [gamma0, gamma1, gamma2, gamma3];
+    
+    //Elektronenmasse in MeV/c^2 (natürliche Einheiten)
+    let mass = 0.511; 
+
+    // Elektronenladung in natürlichen Einheiten
+    let e = 1.0; 
+    
+    // Definieren Sie hier ggf. das externe Potenzial:
+    
     for i in 0..4 {
         println!("Gamma{} =\n{}", i, gamma_matrix[i]);
     }
+    
+    println!("Teilchenmasse: {} MeV/c^2", mass);
+    println!("Teilchenladung: {} natürliche Belastungseinheiten", e);
 }
